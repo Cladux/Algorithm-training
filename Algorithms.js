cdab9f14f298699b50c36bc5e1cd86e24155be09
Soroush Sharifi <sharifysoroush@gmail.com>
2024-02-20 14:40:56 +0330

    add Selection Sort

--- Algorithms.js
+++ Algorithms.js
@@ -1,15 +1,14 @@
-// Binary Search
-
-function binarySearch(array, target) {
+// ------ Binary Search ------
+function binarySearch(arr, target) {
   let left = 0;
-  let right = array.length - 1;
+  let right = arr.length - 1;
   let bigO = 0;
 
   while (left <= right) {
     const mid = Math.floor((left + right) / 2);
 
     // Check if target is present at mid
-    if (array[mid] === target) {
+    if (arr[mid] === target) {
       bigO++;
       return console.log(
         `Element ${target} found at index ${mid}. and big O is: ${bigO}`
@@ -17,7 +16,7 @@ function binarySearch(array, target) {
     }
 
     // If target is greater, ignore left half
-    if (array[mid] < target) {
+    if (arr[mid] < target) {
       bigO++;
       left = mid + 1;
     } else {
@@ -30,6 +29,36 @@ function binarySearch(array, target) {
   return console.log(`Element ${target} not found in the array.`);
 }
 
-const arr = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+// Example usage:
+const binaryArray = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
 const target = 14;
-binarySearch(arr, target);
+
+// binarySearch(binaryArray, target);
+
+// ------ Selection Sort ------
+function selectionSort(arr) {
+  const n = arr.length;
+  const sortedArray = [];
+
+  // Traverse through all array elements
+  for (let i = 0; i < n; i++) {
+    // Find the minimum element in the remaining unsorted array
+    let minIndex = 0;
+    for (let j = 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    // Push the minimum element to the sorted array and remove it from the original array
+    sortedArray.push(arr[minIndex]);
+    arr.splice(minIndex, 1);
+  }
+
+  return sortedArray;
+}
+
+// Example usage:
+const arr = [64, 25, 12, 22, 11];
+console.log("Original array:", arr);
+console.log("Sorted array:", selectionSort(arr));
